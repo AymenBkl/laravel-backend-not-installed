9c0f12814c17bc731821e1d607c8bfda5aad91aa
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2020-10-23 00:35:13 +0200

    new version

--- app/Models/AppModels/Product.php
+++ app/Models/AppModels/Product.php
@@ -561,8 +561,9 @@ class Product extends Model
                     $categories->where('products.products_liked', '>', 0);
                 }
 
-                    $categories->where('products.products_liked', '=', $zone_id);
-                
+                if ($zone_id != null && $zone_id != ''){
+                    $categories->where('products.zone_id', '=', $zone_id);
+                }
                 //wishlist customer id
                 if ($type == "wishlist") {
                     $categories->where('liked_customers_id', '=', $request->customers_id);
